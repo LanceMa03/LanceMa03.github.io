b7c7f7f955272f4980a6cdb347c39dd711b99cee
Lance1738 <71152123+Lance1738@users.noreply.github.com>
2021-12-30 20:51:04 -0800

    navbar scroll

---
+++ script.js
@@ -0,0 +1,10 @@
+headerHeight = $("header").height();
+
+$(window).on("scroll", function () {
+  if ($(window).scrollTop()) {
+      $('nav').addClass('black');
+  }  
+  else {
+      $('nav').removeClass('black');
+  }
+})
